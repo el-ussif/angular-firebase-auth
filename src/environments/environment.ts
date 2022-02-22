31c69b20c15035900ce0319b11e097a9ac799ee3
El Youssif Awanou <elyoussifaw@gmail.com>
2022-02-22 09:46:54 +0000

    set up for BIDOFI Project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,14 @@ export const environment = {
   endpoint: 'API_URL/api/v1',
   server_host: 'API_URL',
   firebaseConfig: {
-    databaseURL: "https://piam-erp-default-rtdb.firebaseio.com",
-    apiKey: "AIzaSyC2uRM_OIVmA0lymUgwbIU69dqLq11ajlU",
-    authDomain: "piam-erp.firebaseapp.com",
-    projectId: "piam-erp",
-    storageBucket: "piam-erp.appspot.com",
-    messagingSenderId: "208967734392",
-    appId: "1:208967734392:web:075530ce73f35320b3498d"
+    databaseURL: "https://bidofi-web-app-default-rtdb.firebaseio.com",
+    apiKey: "AIzaSyCicHZgzlMoWX19t2ursWZzJivaz6atc1Y",
+    authDomain: "bidofi-web-app.firebaseapp.com",
+    projectId: "bidofi-web-app",
+    storageBucket: "bidofi-web-app.appspot.com",
+    messagingSenderId: "37163159579",
+    appId: "1:37163159579:web:be202f7bb78c665db6e8be",
+    measurementId: "G-179WBMXWXS",
   },
 };
 
